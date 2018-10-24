b2d78b4eb24efe1c5186e0a14938f9e187266afe
jhaff <jacobhaff2@gmail.com>
2018-10-23 17:19:33 -0700

    manually shuffling command line arguments

--- rearrange.py
+++ rearrange.py
@@ -1,7 +1,17 @@
+"""
+Randomly shuffles command line arguments
+"""
+
 import sys #access command line arguments
 import random # for generating random numbers
 
+def rearrange_words(words):
+    for i in range(len(words)):
+        random_index = random.randrange(len(words))
+        print(words[random_index])
+        del words[random_index]
+
 if __name__ == '__main__':
-    params= sys.argv[1:] # take a sublist starting from index 1 till the end
-    random.shuffle(params)
+    params = sys.argv[1:] # take a sublist starting from index 1 till the end
+    rearrange_words(params)
     print(params)
