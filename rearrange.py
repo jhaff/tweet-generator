d5802da69cecf270b7bfae9631aaa005ed8e12c6
jhaff <jacobhaff2@gmail.com>
2018-10-25 16:04:38 -0700

    fixed minor bug in rearrange.py

--- rearrange.py
+++ rearrange.py
@@ -14,4 +14,3 @@ def rearrange_words(words):
 if __name__ == '__main__':
     params = sys.argv[1:] # take a sublist starting from index 1 till the end
     rearrange_words(params)
-    print(params)
